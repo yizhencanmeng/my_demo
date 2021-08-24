b5f612934f979c043ecc66f15e0a8685494a9cfb
fanjiangtao <1434005852@qq.com>
2021-08-24 22:00:34 +0800

    one

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '../public/iconfont/iconfont.css'
 
 Vue.config.productionTip = false
+
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
 
 new Vue({
   router,
