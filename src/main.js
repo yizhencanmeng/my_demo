962bca5a19fa1a2342d7f71cb7ecb1def314a005
zhangzuodong <1814178015@qq.com>
2021-08-25 07:43:23 +0800

    添加的装修预算

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import "../public/iconfont/iconfont.css"
 Vue.config.productionTip = false
-
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
 new Vue({
   router,
   store,
