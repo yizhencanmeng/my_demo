ce81af8306f29216acd03d47539a4a536c263867
fanjiangtao <1434005852@qq.com>
2021-08-25 11:06:11 +0800

    456789

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Hyh from '../views/Hyh.vue'
+import Hyh from '../views/hyh.vue'
 
 Vue.use(VueRouter)
 
