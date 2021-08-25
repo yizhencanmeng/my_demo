6ad12922ef9d6bb1edf28e66ca06a37ebf1b8059
heyuhang <1337535208@.qq.com>
2021-08-25 08:22:00 +0800

    yi

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import hyh from '../views/hyh.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/hyh',
+    name: 'hyh',
+    component: hyh
   },
   {
     path: '/zx',
