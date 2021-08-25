5e9f9099c49a00aa22a9306344b3217e5608732a
chenghao <1947580224>
2021-08-25 11:48:23 +0800

    123zxc

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,54 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import hyh from '../views/hyh.vue'
-import zs from '../views/Zs.vue'
-import lxg from '../views/Lxg.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import hyh from "../views/hyh.vue";
+import zs from "../views/Zs.vue";
+import lxg from "../views/Lxg.vue";
+import xgt from "../views/xgt.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-      path: '/Hyh',
-      name: 'Hyh',
-      component:hyh
-    },
+const routes = [
   {
-        path: '/Zs',
-        name: 'Zs',
-        component:zs
-      },
-    {
-          path: '/lxg',
-          name: 'lxg',
-          component:lxg
-        },
-  {
-    path: '/zx',
-    name: 'zx',
-    component: () => import( '../views/zx.vue')
-  },
-  { 
-    path: '/zx',
-    redirect:"/zx"
-  }
-]
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/Hyh",
+    name: "Hyh",
+    component: hyh,
+  },
+  {
+    path: "/xgt",
+    name: "xgt",
+    component: xgt,
+  },
+  {
+    path: "/Zs",
+    name: "Zs",
+    component: zs,
+  },
+  {
+    path: "/lxg",
+    name: "lxg",
+    component: lxg,
+  },
+  {
+    path: "/zx",
+    name: "zx",
+    component: () => import("../views/zx.vue"),
+  },
+  {
+    path: "/zx",
+    redirect: "/zx",
+  },
+];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+export default router;
